df08fbbf1089398da974624b0374c345a34061f0
Andrew Donnelly <andrew.donnelly.1403@gmail.com>
2025-08-11 15:35:40 +0100

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,24 @@ export default function RootLayout({
           name="description"
           content="Professional full-stack web solutions by Andrew Donnelly, using Next.js, MUI, and TypeScript. Innovative, responsive, Gryffindor-inspired design."
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
 
       <body>
